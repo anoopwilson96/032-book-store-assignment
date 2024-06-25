e6a85f2a782ea4df9a6f84851acf48421a4e3ab9
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-25 15:29:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Home from './routes/home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      }
+
+    ]
+  }
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
