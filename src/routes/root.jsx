3dc5cdf4595610af2646ed427fbe34357e9e4bc5
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-26 22:15:28 +0530

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,32 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Root() {
   return (
     <>
 
-    <header>
-      <>
-        <section className="headerOne">
-          <img className="  max-h-48 mx-auto" src="./logo.png" alt="" />
-        </section>
-        <section className="headerTwo mt-0">
-          <div className="searchBar  flex flex-row justify-center ">
-          <input type="text" className="border border-red-600  w-5/12 outline-none p-1" placeholder="Search by authors,book name,language & categories " />
-          <button className="bg-red-600 px-2"><span className="material-symbols-outlined  text-white  text-lg">search</span>
-          </button>
-          
-          </div>
-        </section>
-      </>
-    </header>
 
+    <Header/>
     <Outlet/>
+    <Footer/>
 
-    <footer>
-      <h1>
-        hello world Its me FOOTER
-      </h1>
-    </footer>
+
     </>
       );
     }
