d0955d1ed35538ee8f7c075bc60ba5a3381932fb
Anoop Wilson <anoopwilson33@yahoo.com>
2024-06-28 11:32:09 +0530

    Update home.jsx

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -10,7 +10,12 @@ export async function loader() {
   
     const response = await fetch('http://localhost:3000/authors')
     const authors = await response.json()
-    return { authors}
+
+    const bookResponse = await fetch('http://localhost:3000/books')
+    const books = await bookResponse.json()
+    const slicedBooks = books.slice(0, 15); // Get the first 15 elements
+  
+    return { authors,slicedBooks}
   } 
 
 
@@ -19,13 +24,13 @@ export async function loader() {
 
 
 export default function Home() {
-  const {authors} = useLoaderData();
+  const {authors,slicedBooks} = useLoaderData();
   return (
 <>
 
 <ImageSlider/>
 
-<section>
+<section className='Home-Popular-Authors-Section'>
 
 <div>
       <h1 className='mt-5 mb-10 font-bold text-4xl flex justify-center'>
@@ -39,13 +44,47 @@ export default function Home() {
     {
     authors.map((author)=>(
 
-    <div key={author._id} className='mx-auto border border-red-600 rounded-3xl shadow-slate-400 p-5 flex flex-col  items-center text-wrap max-w-56' >
+    <div key={author._id} className='mx-auto border border-red-600 rounded-3xl shadow-slate-400 p-5 flex flex-col  items-center text-wrap  max-w-3xl' >
       <img className='h-10 rounded-full' src={author.image} alt="" />
       <h3 className='mt-5 mb-5 text-2xl font-semibold'>{author.authorName}</h3>
       <span className='text-lg'>{author.details}</span>
     </div>
 
     ))} </div>
+
+
+</section>
+
+<section className="Home-Popular-Books-Section">
+
+
+<div>
+      <h1 className='mt-5 mb-10 font-bold text-4xl flex justify-center'>
+        Popular Books
+      </h1>
+</div>
+
+  
+<div className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-auto  '>
+  {slicedBooks.map((book)=>(
+    <div key={book._id} className='flex justify-center '>
+     <div className=' border border-red-600 rounded-3xl shadow-slate-400 p-5 flex flex-col items-center justify-center text-wrap max-w-56' >
+        <img className='' src={book.bookImage} alt="" />
+        <h3 className='mt-5  text-2xl font-semibold'>{book.bookName}</h3>
+        <h3 className=' mb-2 text-lg text-blue-800 '>{book.bookAuthor}</h3>
+        <span className='text-lg'> Price: $ {book.price} </span>
+        <div className='flex flex-col'>
+          <button className=' mt-3 mb-2 bg-red-500 text-white px-2 rounded-md'>Add to Cart</button>
+          <button className=' mt-3 mb-2 bg-slate-400  text-white px-2 rounded-md'>Mark ❤️️</button>
+        </div>
+      </div>
+   </div>   
+      ))}
+</div>
+
+
+
+
 
 
 </section>
