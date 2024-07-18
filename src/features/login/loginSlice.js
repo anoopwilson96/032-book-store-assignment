9d1d8a24f56d0636d37b150e9f0ca1362f0e809b
Anoop Wilson <anoopwilson33@yahoo.com>
2024-07-18 08:33:21 +0530

    Created loginSlice for redux

---
+++ src/features/login/loginSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+// get codes from redux quick start-toolkit
+//prepare loginSlice(we need global state to manage Login/Logout status)
+export const loginSlice = createSlice({
+  name: 'login',
+  initialState: {  
+    userLoggedIn: false //indicates first value of data.Here its user is LoggedOut so useLoggedIn : false
+  },
+
+  reducers: {
+// here state means current value and action means next value or next change that we don't know now.
+    setUserLoggedIn:(state,action ) =>{
+        state.userLoggedIn = action.payload
+    //so new state of userLoggedIn is equal to action.payload
+    }
+
+  }
+})
+
+// Action creators are generated for each case reducer function
+
+export const { setUserLoggedIn } = loginSlice.actions
+//now export our reducer, so it can be used in different part of website
+export default loginSlice.reducer
