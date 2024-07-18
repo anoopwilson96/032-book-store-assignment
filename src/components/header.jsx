d82f72e5820421f5a683c051a67bff63ad97351a
Anoop Wilson <anoopwilson33@yahoo.com>
2024-07-18 08:32:59 +0530

    added redux codes
    Used useDispatch() and useSelect()

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,28 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, Link } from "react-router-dom";
+import { setUserLoggedIn } from '../features/login/loginSlice';
 
 export default function Header() {
-// to work on Login/logout button and for REDUX.
+//codes of REDUX (setLoggedIn used in below useEffect()hook will replace by Redux global state)
+//1st useSelector is hook in redux that help to get or select data 
+const userLoggedIn = useSelector(state =>
+  state.login.userLoggedIn)
+//useSelector will take function state and from state > from login(alias loginSlice) our needed data userLoggedIn is selected
+//(state->login->userLoggedIn)
+//useSelector select the data from Redux store and loginSlice
+// 2nd useDispatch() hook of redux will call Reducer 
+const dispatch = useDispatch()
+// and give data(new State) (logged in or not from useEffect()/old local state request )
+//the userLoggedIn Global State of redux will replace loggedIn and does conditional rendering
+
+
+
+
+// to work on Login/logout button (its a local state)REDUX WILL EDIT IT LATER
 //when page is loaded, loggedIn is false
-const [loggedIn,setLoggedIn]= useState(false)
-
-
-
-
+const [loggedIn,setLoggedIn]= useState(false)  //HERE setLoggedIn is LOCAL STATE. IT WILL BE REPLACED BY GLOBAL SATE
 
 // CODE FOR LOGIN & LOGOUT STATUS ON TOP AND REDUX
 //useEffect is used because we need it to load after page is loaded
@@ -19,11 +32,14 @@ useEffect(()=>{
   axios.get(`${import.meta.env.VITE_API_URL}/auth/verify`,{withCredentials:true})
   .then(response =>{
     console.log(response)
-    setLoggedIn(true)  //now if user is logged in, it will setLoggedIn as true 
+    //setLoggedIn(true) [replaced by GlobalState]  //now if user is logged in, it will setLoggedIn as true 
     //and at bottom conditional rendering is used to show either LOGIN or LOGOUT
+    dispatch(setUserLoggedIn(true))
+    //here redux useDispatch is used to update userLoggedIn state
   })
   .catch(error=>{
     console.log('User not signed in')
+    dispatch(setUserLoggedIn(false))
   })
 })
 
@@ -74,8 +90,8 @@ useEffect(()=>{
             </Link>
 {/* conditional rendering is used to determine to show LOGOUT or LOGIN. if setLoggedIn is true, it will show LOGIN
 or else LOGOUT */}
-            {
-              loggedIn? (<Link to={'/logout'}>
+            { // loggedIn? this localState replaced by globalState of redux 
+             userLoggedIn?  (<Link to={'/logout'}>
                 <li className="hover:text-red-600 text-lg ">Logout</li>
               </Link>):(<Link to={'/login'}>
               <li className="hover:text-red-600 text-lg text-wrap ">Login</li>
