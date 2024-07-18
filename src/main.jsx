fdbfe6e6e57dbd79e691a43100ed3ac72bef046b
Anoop Wilson <anoopwilson33@yahoo.com>
2024-07-18 08:33:30 +0530

    added redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import store from './app/store'
+import { Provider } from 'react-redux'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -64,7 +66,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
 
